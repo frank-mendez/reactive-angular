361ab1ec41ba341a93e1012926d84055fac0d539
Frank Mendez <frankmendezresources@gmail.com>
2021-08-25 16:09:12 +0800

    Angular view Layer Patterns - Smart vs Presentational Components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,10 +15,7 @@ export class HomeComponent implements OnInit {
   beginnerCourses$: Observable<Course[]>;
   advancedCourses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
     const courses$ = this.coursesService
@@ -34,17 +31,5 @@ export class HomeComponent implements OnInit {
         courses.filter((course) => course.category === "ADVANCED")
       )
     );
-  }
-
-  editCourse(course: Course) {
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
   }
 }
